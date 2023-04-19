483e697e00095645861fde6190b07ea74e05924a
Will Ben <wilfrid.benoit@outlook.fr>
2023-04-19 15:19:07 +0200

    first commit with angular directives solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directives';
+
+  title = 'Bonjour !';
+
+  isAdmin: boolean = true;
+
+  changeRole() {
+    this.isAdmin = !this.isAdmin;  
+  }
 }
